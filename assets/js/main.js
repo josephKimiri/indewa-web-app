93e499690d6725156dcf074907b34c965d155d3c
josephKimiri <joseph.kimiri.macharia@gmail.com>
2022-06-09 20:21:58 +0300

    Write the logic for Mobile navigation's dropdown to activate on mobiledevices

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,4 +54,12 @@
         this.classList.toggle('bi-list')
         this.classList.toggle('bi-x')
       })
-    
+      /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
