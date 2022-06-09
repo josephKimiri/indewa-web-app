930e328a8c20d4ea0d2789efe79a800cf9494d6a
josephKimiri <joseph.kimiri.macharia@gmail.com>
2022-06-09 20:20:26 +0300

    Create a javascript mobile navigation toggle to help navihgate the website using mobile phones.

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,3 +46,12 @@
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
+    /**
+   * Mobile nav toggle
+   */
+     on('click', '.mobile-nav-toggle', function(e) {
+        select('#navbar').classList.toggle('navbar-mobile')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+      })
+    
