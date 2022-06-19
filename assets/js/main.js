5f1a2b736b4b85f0b8aa274bade632740f2bf991
josephKimiri <joseph.kimiri.macharia@gmail.com>
2022-06-19 11:40:14 +0300

    Make some changes on the icons colors to orange.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,25 @@
+/**
+* Template Name: Indewa - v4.7.0
+* Template URL: https://bootstrapmade.com/Indewa-free-html-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
 (function() {
-    "use strict";
-  
-    /**
-     * Easy selector helper function
-     */
-    const select = (el, all = false) => {
-      el = el.trim()
-      if (all) {
-        return [...document.querySelectorAll(el)]
-      } else {
-        return document.querySelector(el)
-      }
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
     }
-      /**
+  }
+
+  /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
@@ -25,13 +32,15 @@
       }
     }
   }
-    /**
+
+  /**
    * Easy on scroll event listener 
    */
-     const onscroll = (el, listener) => {
-        el.addEventListener('scroll', listener)
-      }
-        /**
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
    * Back to top button
    */
   let backtotop = select('.back-to-top')
@@ -46,15 +55,17 @@
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
-    /**
+
+  /**
    * Mobile nav toggle
    */
-     on('click', '.mobile-nav-toggle', function(e) {
-        select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
-      })
-      /**
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
    * Mobile nav dropdowns activate
    */
   on('click', '.navbar .dropdown > a', function(e) {
@@ -63,7 +74,20 @@
       this.nextElementSibling.classList.toggle('dropdown-active')
     }
   }, true)
- /**
+
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
+  /**
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
